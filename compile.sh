94f76f5823fe4c697e4c2f47bb152011d8296020
Allen Frank Cutinha <allencutinha@gmail.com>
2022-12-18 11:13:54 +0100

    added the missing header and a script to format, compile and run the samples

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#! usr/bin/bash
+fcode
+cd build
+make
+./exe l
+./exe c
+./exe d
+cd ..
---
+++ line.hpp
@@ -0,0 +1,74 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+
+void drawLine(cv::Mat &img, cv::Point2d const &pt1, cv::Point2d const &pt2) {
+    // Coordinates of the two points
+    int x1 = pt1.x, y1 = pt1.y;
+    int x2 = pt2.x, y2 = pt2.y;
+    double dx = x1 - x2;
+    double dy = y1 - y2;
+
+    int lx = pt1.x;
+    int rx = pt2.x;
+    int ty = pt1.y;
+    int by = pt2.y;
+    if (dx > 0) {
+        lx = pt2.x;
+        rx = pt1.x;
+    }
+    if (dy > 0) {
+        ty = pt2.y;
+        by = pt1.y;
+    }
+
+    if (0 == int(dy)) {
+        for (int x = lx; x <= rx; ++x) {
+            int y = y1;
+            cv::Vec3b &pix = img.at<cv::Vec3b>(x, y);
+            pix = cv::Vec3b(0, 255, 0);
+        }
+    } else if (0 == int(dx)) {
+        for (int y = ty; y <= by; ++y) {
+            int x = x1;
+            cv::Vec3b &pix = img.at<cv::Vec3b>(x, y);
+            pix = cv::Vec3b(0, 255, 0);
+        }
+    } else {
+        // Calculate the slope and y-intercept
+        double m = (dy) / (dx);
+        double b = y1 - m * x1;
+
+        // Estimate the coordinates of the line
+        if (dx < dy) {
+            for (int x = lx; x <= rx; ++x) {
+                int y = m * x + b;
+                cv::Vec3b &pix = img.at<cv::Vec3b>(x, y);
+                pix = cv::Vec3b(0, 255, 0);
+            }
+        } else {
+            for (int y = ty; y <= by; ++y) {
+                int x = (y - b) / m;
+                cv::Vec3b &pix = img.at<cv::Vec3b>(x, y);
+                pix = cv::Vec3b(0, 255, 0);
+            }
+        }
+    }
+}
+
+inline void test_line() {
+    cv::Mat img = cv::Mat::zeros(400, 800, CV_8UC3);
+    // horizontal line
+    drawLine(img, cv::Point2d(200, 300), cv::Point2d(200, 500));
+
+    // vertical line
+    drawLine(img, cv::Point2d(100, 400), cv::Point2d(300, 400));
+
+    // negative slope line
+    drawLine(img, cv::Point2d(100, 500), cv::Point2d(300, 300));
+
+    // positive slope line
+    drawLine(img, cv::Point2d(100, 300), cv::Point2d(300, 500));
+
+    cv::imshow("test-lines", img);
+    cv::waitKey(0);
+}
