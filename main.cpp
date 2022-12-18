ff33ff04c19486825a00f3613d305a374726dc21
Allen Frank Cutinha <allencutinha@gmail.com>
2022-12-18 11:12:07 +0100

    added line drawing functionality and its tests

--- main.cpp
+++ main.cpp
@@ -1,28 +1,25 @@
 #include "circle.hpp"
+#include "line.hpp"
 #include <opencv2/opencv.hpp>
-
-void test_circle() {
-    cv::Mat img = cv::Mat::zeros(400, 800, CV_8UC3);
-    drawCircle(img, cv::Point2d(200, 400), 50);
-    cv::imshow("dekh-le", img);
-    cv::waitKey(0);
-}
 
 void test_rectangle() {}
 
 void help() {
     std::cout << "Enter a valid input option" << std::endl;
+    std::cout << "l - to draw line" << std::endl;
     std::cout << "c - to draw circle" << std::endl;
     std::cout << "r - to draw rectangle" << std::endl;
     exit(2);
 }
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     if (argc < 2) {
         help();
     }
-    if (0 == std::strcmp("c", argv[1])) {
-        test_circle();
+    if (0 == std::strcmp("l", argv[1])) {
+        test_line();
     } else if (0 == std::strcmp("c", argv[1])) {
+        test_circle();
+    } else if (0 == std::strcmp("r", argv[1])) {
 
     } else {
         help();
--- rectangle.hpp
+++ rectangle.hpp
@@ -1,4 +1,4 @@
 #pragma once;
-#include<opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 
-inline void drawRectangle(cv::Point2d const & tl, cv::Point2d const& bl){}
+inline void drawRectangle(cv::Point2d const &tl, cv::Point2d const &bl) {}
