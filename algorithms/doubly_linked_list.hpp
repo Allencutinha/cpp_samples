19716d23b7c9b8da33a723c4b4ff173b928f707a
Allen Frank Cutinha <allencutinha@gmail.com>
2022-12-21 09:30:32 +0100

    added bubble sort and insertion sort

--- algorithms/doubly_linked_list.hpp
+++ algorithms/doubly_linked_list.hpp
@@ -2,105 +2,106 @@
 #include <iostream>
 
 class Node {
- public:
-  int data;
-  Node* next;
-  Node* prev;
-  Node(int data) {
-    this->data = data;
-    this->next = nullptr;
-    this->prev = nullptr;
-  }
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+        this->prev = nullptr;
+    }
 };
 
 class DoublyLinkedList {
- public:
-  Node* head;
-  Node* tail;
-  int size;
-  DoublyLinkedList() {
-    head = nullptr;
-    tail = nullptr;
-    size = 0;
-  }
+  public:
+    Node *head;
+    Node *tail;
+    int size;
+    DoublyLinkedList() {
+        head = nullptr;
+        tail = nullptr;
+        size = 0;
+    }
 
-  // Insert a new node at the front of the list
-  void pushFront(int data) {
-    Node* newNode = new Node(data);
-    newNode->next = head;
-    if (head != nullptr) {
-      head->prev = newNode;
+    // Insert a new node at the front of the list
+    void pushFront(int data) {
+        Node *newNode = new Node(data);
+        newNode->next = head;
+        if (head != nullptr) {
+            head->prev = newNode;
+        }
+        head = newNode;
+        if (tail == nullptr) {
+            tail = newNode;
+        }
+        size++;
     }
-    head = newNode;
-    if (tail == nullptr) {
-      tail = newNode;
-    }
-    size++;
-  }
 
-  // Insert a new node at the back of the list
-  void pushBack(int data) {
-    Node* newNode = new Node(data);
-    newNode->prev = tail;
-    if (tail != nullptr) {
-      tail->next = newNode;
+    // Insert a new node at the back of the list
+    void pushBack(int data) {
+        Node *newNode = new Node(data);
+        newNode->prev = tail;
+        if (tail != nullptr) {
+            tail->next = newNode;
+        }
+        tail = newNode;
+        if (head == nullptr) {
+            head = newNode;
+        }
+        size++;
     }
-    tail = newNode;
-    if (head == nullptr) {
-      head = newNode;
-    }
-    size++;
-  }
 
-  // Remove the node at the front of the list
-  void popFront() {
-    if (head == nullptr) {
-      return;
+    // Remove the node at the front of the list
+    void popFront() {
+        if (head == nullptr) {
+            return;
+        }
+        Node *temp = head;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        }
+        delete temp;
+        size--;
     }
-    Node* temp = head;
-    head = head->next;
-    if (head != nullptr) {
-      head->prev = nullptr;
-    }
-    delete temp;
-    size--;
-  }
 
-  // Remove the node at the back of the list
-  void popBack() {
-    if (tail == nullptr) {
-      return;
+    // Remove the node at the back of the list
+    void popBack() {
+        if (tail == nullptr) {
+            return;
+        }
+        Node *temp = tail;
+        tail = tail->prev;
+        if (tail != nullptr) {
+            tail->next = nullptr;
+        }
+        delete temp;
+        size--;
     }
-    Node* temp = tail;
-    tail = tail->prev;
-    if (tail != nullptr) {
-      tail->next = nullptr;
-    }
-    delete temp;
-    size--;
-  }
 
-  // Print the contents of the list
-  void printList() {
-    Node* current = head;
-    while (current != nullptr) {
-      std::cout << current->data << " ";
-      current = current->next;
+    // Print the contents of the list
+    void printList() {
+        Node *current = head;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-  }
 };
-
+namespace dll {
 int test() {
-  DoublyLinkedList list;
-  list.pushBack(1);
-  list.pushBack(2);
-  list.pushBack(3);
-  list.pushFront(0);
-  list.printList();  // Output: 0 1 2 3
-  list.popFront();
-  list.printList();  // Output: 1 2 3
-  list.popBack();
-  list.printList();  // Output: 1 2
-  return 0;
+    DoublyLinkedList list;
+    list.pushBack(1);
+    list.pushBack(2);
+    list.pushBack(3);
+    list.pushFront(0);
+    list.printList(); // Output: 0 1 2 3
+    list.popFront();
+    list.printList(); // Output: 1 2 3
+    list.popBack();
+    list.printList(); // Output: 1 2
+    return 0;
 }
+} // namespace dll
--- algorithms/singly_linked_list.hpp
+++ algorithms/singly_linked_list.hpp
@@ -2,108 +2,112 @@
 
 #include <cstddef>
 
-template <typename T>
-class SinglyLinkedList {
- public:
-  SinglyLinkedList() : head_(NULL), tail_(NULL), size_(0) {}
+template <typename T> class SinglyLinkedList {
+  public:
+    SinglyLinkedList() : head_(NULL), tail_(NULL), size_(0) {}
 
-  // Check if the list is empty
-  bool empty() const { return size_ == 0; }
+    // Check if the list is empty
+    bool empty() const { return size_ == 0; }
 
-  // Get the number of elements in the list
-  std::size_t size() const { return size_; }
+    // Get the number of elements in the list
+    std::size_t size() const { return size_; }
 
-  // Get the element at the front of the list
-  const T& front() const { return head_->data; }
+    // Get the element at the front of the list
+    const T &front() const { return head_->data; }
 
-  // Get the element at the back of the list
-  const T& back() const { return tail_->data; }
+    // Get the element at the back of the list
+    const T &back() const { return tail_->data; }
 
-  // Insert an element at the front of the list
-  void insert_front(const T& data) {
-    Node* node = new Node(data, head_);
-    if (head_ == NULL) {
-      head_ = node;
-      tail_ = node;
-    } else {
-      head_ = node;
+    // Insert an element at the front of the list
+    void insert_front(const T &data) {
+        Node *node = new Node(data, head_);
+        if (head_ == NULL) {
+            head_ = node;
+            tail_ = node;
+        } else {
+            head_ = node;
+        }
+        ++size_;
     }
-    ++size_;
-  }
 
-  // Insert an element at the back of the list
-  void insert_back(const T& data) {
-    Node* node = new Node(data);
-    if (tail_ == NULL) {
-      head_ = node;
-      tail_ = node;
-    } else {
-      tail_->next = node;
-      tail_ = node;
+    // Insert an element at the back of the list
+    void insert_back(const T &data) {
+        Node *node = new Node(data);
+        if (tail_ == NULL) {
+            head_ = node;
+            tail_ = node;
+        } else {
+            tail_->next = node;
+            tail_ = node;
+        }
+        ++size_;
     }
-    ++size_;
-  }
 
-  // Remove the element at the front of the list
-  void remove_front() {
-    if (head_ == NULL) return;
-    Node* temp = head_;
-    head_ = head_->next;
-    delete temp;
-    --size_;
-    if (head_ == NULL) tail_ = NULL;
-  }
-
-  // Remove the element at the back of the list
-  void remove_back() {
-    if (tail_ == NULL) return;
-    if (head_ == tail_) {
-      delete head_;
-      head_ = NULL;
-      tail_ = NULL;
-      return;
+    // Remove the element at the front of the list
+    void remove_front() {
+        if (head_ == NULL)
+            return;
+        Node *temp = head_;
+        head_ = head_->next;
+        delete temp;
+        --size_;
+        if (head_ == NULL)
+            tail_ = NULL;
     }
-    Node* temp = head_;
-    while (temp->next != tail_) temp = temp->next;
-    delete tail_;
-    tail_ = temp;
-    tail_->next = NULL;
-    --size_;
-  }
 
-  // Search for an element in the list
-  bool search(const T& data) const {
-    Node* temp = head_;
-    while (temp != NULL) {
-      if (temp->data == data) return true;
-      temp = temp->next;
+    // Remove the element at the back of the list
+    void remove_back() {
+        if (tail_ == NULL)
+            return;
+        if (head_ == tail_) {
+            delete head_;
+            head_ = NULL;
+            tail_ = NULL;
+            return;
+        }
+        Node *temp = head_;
+        while (temp->next != tail_)
+            temp = temp->next;
+        delete tail_;
+        tail_ = temp;
+        tail_->next = NULL;
+        --size_;
     }
-    return false;
-  }
 
-  // Clear the list
-  void clear() {
-    Node* temp = head_;
-    while (temp != NULL) {
-      Node* next = temp->next;
-      delete temp;
-      temp = next;
+    // Search for an element in the list
+    bool search(const T &data) const {
+        Node *temp = head_;
+        while (temp != NULL) {
+            if (temp->data == data)
+                return true;
+            temp = temp->next;
+        }
+        return false;
     }
-    head_ = NULL;
-    tail_ = NULL;
-    size_ = 0;
-  }
 
- private:
-  // Node for the singly linked list
-struct Node {
-  T data;
-  Node* next;
-  Node(const T& data) : data(data), next(NULL) {}
-  Node(const T& data, Node* next) : data(data), next(next) {}
-};
+    // Clear the list
+    void clear() {
+        Node *temp = head_;
+        while (temp != NULL) {
+            Node *next = temp->next;
+            delete temp;
+            temp = next;
+        }
+        head_ = NULL;
+        tail_ = NULL;
+        size_ = 0;
+    }
 
-  Node * tail_;
-  Node * head_;
-  int size_ ;
+  private:
+    // Node for the singly linked list
+    struct Node {
+        T data;
+        Node *next;
+        Node(const T &data) : data(data), next(NULL) {}
+        Node(const T &data, Node *next) : data(data), next(next) {}
+    };
+
+    Node *tail_;
+    Node *head_;
+    int size_;
 };
