ab8e012b066234204a6149fc75a620787abbbef1
Allen Cutinha <allencutinha@gmail.com>
2023-01-17 11:38:08 +0100

    format

--- cpp/array/custom_std_array.cpp
+++ cpp/array/custom_std_array.cpp
@@ -1,5 +1,5 @@
 #include <cstdint>
-#include<iostream>
+#include <iostream>
 
 template <typename Contained, std::size_t Size>
 struct array {
@@ -16,14 +16,14 @@ struct array {
     constexpr const Contained* end() const { return _elements + Size; }
     constexpr const Contained* ccend() const { return _elements + Size; }
 
-    constexpr const Contained& front() const{return *_elements;}
-    constexpr Contained& front(){return *_elements;}
+    constexpr const Contained& front() const { return *_elements; }
+    constexpr Contained& front() { return *_elements; }
 };
 
 int main(int argc, char** argv)
 {
-    array <int, 5> data{1,2,3,4,5};
-    for(auto val: data){
-        std::cout<<val<<std::endl;
+    array<int, 5> data{1, 2, 3, 4, 5};
+    for (auto val: data) {
+        std::cout << val << std::endl;
     }
 }
