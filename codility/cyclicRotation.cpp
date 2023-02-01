c7979bc9836a3872a673211f0bc87891f656e544
Allen Cutinha <allencutinha@gmail.com>
2023-02-01 18:03:38 +0100

    formatted code

--- codility/cyclicRotation.cpp
+++ codility/cyclicRotation.cpp
@@ -62,12 +62,17 @@ vector<int> solution(vector<int>& A, int K)
     return B;
 }
 
-vector<int> solution2(vector<int> &A, int K) {
+vector<int> solution2(vector<int>& A, int K)
+{
     // Implement your solution here
     int N = A.size();
-    if(0 == K || 0 == N || 0 == K%N){ return A;}
+    if (0 == K || 0 == N || 0 == K % N) {
+        return A;
+    }
     vector<int> B = A;
-    K = K%N;
-    for(int i = 0; i<N;++i){B[(K+i)%N] = A[i];}
+    K = K % N;
+    for (int i = 0; i < N; ++i) {
+        B[(K + i) % N] = A[i];
+    }
     return B;
 }
