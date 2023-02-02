41a217cfc3d9d42d72b3f49c0bf491ecba9b3d70
Allen Cutinha <allencutinha@gmail.com>
2023-02-02 11:06:29 +0100

    formatted

--- leetcode/twoSum.cpp
+++ leetcode/twoSum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
         unordered_map<int, int> map;
-        
+
         int N = nums.size();
 
-        for (int i = 0; i < N; i++)
-        {
-            if (map.count(target - nums[i]))
-            {
+        for (int i = 0; i < N; i++) {
+            if (map.count(target - nums[i])) {
                 return vector{i, map[target - nums[i]]};
             }
 
