8fced08cedff3421c297f72190fcb3d6c44f5ed2
Allen Cutinha <allencutinha@gmail.com>
2023-02-02 11:05:51 +0100

    leet code solution for two sum

---
+++ leetcode/twoSum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        
+        int N = nums.size();
+
+        for (int i = 0; i < N; i++)
+        {
+            if (map.count(target - nums[i]))
+            {
+                return vector{i, map[target - nums[i]]};
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return vector<int>{};
+    }
+};
