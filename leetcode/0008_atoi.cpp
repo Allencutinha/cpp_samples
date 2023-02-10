4ac9e1c412cf3483d8fa05b019cf1927951b90d2
Allen Cutinha <allencutinha@gmail.com>
2023-02-10 09:51:47 +0100

    formatted the code

--- leetcode/0008_atoi.cpp
+++ leetcode/0008_atoi.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    int myAtoi(string str) {
-    long res=0;
-        int sign=1;
-        bool start=false;
-        for (int i=0; i<str.length(); i++){
-            if (str[i]=='-'||str[i]=='+'){
-                start=true;
-                if (str[i]=='-'){
-                    sign=-1;
+    int myAtoi(string str)
+    {
+        long res = 0;
+        int sign = 1;
+        bool start = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == '-' || str[i] == '+') {
+                start = true;
+                if (str[i] == '-') {
+                    sign = -1;
                 }
                 i++;
             }
-            if (str[i]==' '&& !start){
+            if (str[i] == ' ' && !start) {
                 continue;
             }
-            if (str[i]<'0' || str[i]>'9'){
-                 
-                break;}
-            else{
-                start=true;
-                res=res*10+(str[i]-'0');
-                if (res*sign>INT_MAX)
+            if (str[i] < '0' || str[i] > '9') {
+
+                break;
+            } else {
+                start = true;
+                res = res * 10 + (str[i] - '0');
+                if (res * sign > INT_MAX)
                     return INT_MAX;
-                else if (res*sign<INT_MIN)
+                else if (res * sign < INT_MIN)
                     return INT_MIN;
-                if (str[i+1]<'0'||str[i+1]>'9') break;
+                if (str[i + 1] < '0' || str[i + 1] > '9')
+                    break;
             }
         }
-        return res*sign;
+        return res * sign;
     }
 };
