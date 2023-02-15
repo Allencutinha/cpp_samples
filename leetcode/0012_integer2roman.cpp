f539a5b662812dd5ae996c2612f5cd46afea0e1e
Allen Cutinha <allencutinha@gmail.com>
2023-02-15 07:06:19 +0100

    formatted

--- leetcode/0012_integer2roman.cpp
+++ leetcode/0012_integer2roman.cpp
@@ -14,16 +14,20 @@ L             50
 C             100
 D             500
 M             1000
-For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which
+is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
-Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is
+not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making
+four. The same principle applies to the number nine, which is written as IX. There are six instances where
+subtraction is used:
 
-I can be placed before V (5) and X (10) to make 4 and 9. 
-X can be placed before L (50) and C (100) to make 40 and 90. 
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given an integer, convert it to a roman numeral.
 
- 
+
 
 Example 1:
 
@@ -40,7 +44,7 @@ Example 3:
 Input: num = 1994
 Output: "MCMXCIV"
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
- 
+
 
 Constraints:
 
@@ -55,19 +59,19 @@ Acceptance Rate
 
 class Solution {
 public:
-    string intToRoman(int num) {
-    string symbol[] =   {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};    
-    int value[]     =   {1000,900,500,400, 100, 90,  50, 40,  10, 9,   5,  4,   1}; 
-    string result;
+    string intToRoman(int num)
+    {
+        string symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string result;
 
-    for(int i=0; num!=0; i++){
-        while(num >= value[i]){
-            num -= value[i];
-            result+=symbol[i];
+        for (int i = 0; num != 0; i++) {
+            while (num >= value[i]) {
+                num -= value[i];
+                result += symbol[i];
+            }
         }
-    }
 
-    return result; 
+        return result;
     }
 };
-
