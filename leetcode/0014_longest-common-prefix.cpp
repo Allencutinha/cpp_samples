f8d84f206bca11762ee08bf060dfd79d1be8fb47
Allen Cutinha <allencutinha@gmail.com>
2023-02-17 06:25:46 +0100

    formatted

--- leetcode/0014_longest-common-prefix.cpp
+++ leetcode/0014_longest-common-prefix.cpp
@@ -8,7 +8,7 @@ Write a function to find the longest common prefix string amongst an array of st
 
 If there is no common prefix, return an empty string "".
 
- 
+
 
 Example 1:
 
@@ -19,7 +19,7 @@ Example 2:
 Input: strs = ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
- 
+
 
 Constraints:
 
@@ -30,26 +30,25 @@ strs[i] consists of only lowercase English letters.
 
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-    // Sort the given array
-    int n = strs.size();
-    sort(strs.begin(), strs.end());
- 
-    // Find the minimum length from
-    // first and last string
-    int en = min(strs[0].size(),
-                 strs[n - 1].size());
- 
-    // Now the common prefix in first and
-    // last string is the longest common prefix
-    string first = strs[0], last = strs[n - 1];
-    int i = 0;
-    while (i < en && first[i] == last[i])
-        i++;
- 
-    string pre = first.substr(0, i);
-    return pre;
+    string longestCommonPrefix(vector<string>& strs)
+    {
+        // Sort the given array
+        int n = strs.size();
+        sort(strs.begin(), strs.end());
 
+        // Find the minimum length from
+        // first and last string
+        int en = min(strs[0].size(), strs[n - 1].size());
+
+        // Now the common prefix in first and
+        // last string is the longest common prefix
+        string first = strs[0], last = strs[n - 1];
+        int i = 0;
+        while (i < en && first[i] == last[i])
+            i++;
+
+        string pre = first.substr(0, i);
+        return pre;
     }
 };
 
@@ -57,16 +56,18 @@ public:
 
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-    int n = strs.size();
-    sort(strs.begin(), strs.end());
-    int en = min((strs.begin())->size(), (strs.begin() +n-1)->size());
-    string first = strs[0], last = strs[n - 1];
-    int i = 0;
-    while (i < en && first[i] == last[i]){ i++;}
- 
-    string pre = first.substr(0, i);
-    return pre;
+    string longestCommonPrefix(vector<string>& strs)
+    {
+        int n = strs.size();
+        sort(strs.begin(), strs.end());
+        int en = min((strs.begin())->size(), (strs.begin() + n - 1)->size());
+        string first = strs[0], last = strs[n - 1];
+        int i = 0;
+        while (i < en && first[i] == last[i]) {
+            i++;
+        }
 
+        string pre = first.substr(0, i);
+        return pre;
     }
 };
