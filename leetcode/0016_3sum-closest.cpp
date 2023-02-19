18a08bc46c423a091f4ce4a0f3502ffe405d5651
Allen Cutinha <allencutinha@gmail.com>
2023-02-19 08:55:27 +0100

    3sum cloasest solution

---
+++ leetcode/0016_3sum-closest.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int ans=1e6;
+        for(int i=0;i<nums.size();i++)
+        {
+            int tar=nums[i];
+            int s=i+1;
+            int l=nums.size()-1;
+            while(s<l)
+            {
+                int t=tar+nums[s]+nums[l];
+                if(t>target)
+                {
+                    --l;
+                }
+                else if(t<target)
+                {
+                    ++s;
+                }
+                else
+                {
+                    return(target);
+                }
+                if(abs(target-ans)>abs(target-t))
+                {
+                    ans=t;
+                }
+            }
+        }
+        return(ans);
+    }
+};
